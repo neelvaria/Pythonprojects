355142df70768bfa6f554183dd814933aae7a9b9
neelvaria <neelvaria08@gmail.com>
2024-06-21 22:33:33 +0530

    frontenedcompleted

--- to-dolist/todolist_app/urls.py
+++ to-dolist/todolist_app/urls.py
@@ -3,5 +3,13 @@ from .import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('signup', views.signup, name='signup'),
+    path('signup/', views.signup, name='signup'),
+    path('signin/', views.signin, name='signin'),
+    path('addcategory/', views.addcategory, name='addcategory'),
+    path('completed/', views.completed, name='completed'),
+    path('edit/', views.edit, name='edit'),
+    path('forgetpassword/', views.forgetpassword, name='forgetpassword'),
+    path('profile/', views.profile, name='profile'),
+    path('resetpassword/', views.resetpassword, name='resetpassword'),
+    path('tasks_pdf/', views.tasks_pdf, name='tasks_pdf'),
 ]
--- to-dolist/todolist_app/views.py
+++ to-dolist/todolist_app/views.py
@@ -6,3 +6,27 @@ def index(request):
 
 def signup(request):
     return render(request, 'signup.html')
+
+def signin(request):
+    return render(request, 'signin.html')
+
+def addcategory(request):
+    return render(request, 'addcategory.html')
+
+def completed(request):
+    return render(request, 'completed.html')
+
+def edit(request):
+    return render(request, 'edit.html')
+
+def forgetpassword(request):
+    return render(request, 'forgetpassword.html')
+
+def profile(request):
+    return render(request, 'profile.html')
+
+def resetpassword(request):
+    return render(request, 'resetpassword.html')
+
+def tasks_pdf(request):
+    return render(request, 'tasks_pdf.html')
